2b3cad737d40400a0cc937afbb926beb21f47909
Darius <darius.owens.j@gmail.com>
2022-02-25 17:36:20 -0800

    added a js file

---
+++ aps.js
@@ -0,0 +1,28 @@
+console.log("Proof of life!");
+
+// var we are going to use mostly in 102
+ // let we are going to use if the variable is going to change values
+ // const we are going to use if the value will never change
+  // changing values is not the same as updating a value
+
+let userName = "Darius"
+console.log(userName);
+
+ let myName =  prompt("What is your name?");
+ console.log("myName:", myName);
+
+
+ if (myName == "Darius") {
+   document.write("Welcome Wade Wilson chimichangas are ready!")  
+ } else {
+     document.write("Sorry no access here")
+ }
+  document.write(myName);
+
+  if (myName == "Owens") {
+      document.write("The Bacon is Here!") 
+
+      } else {
+          document.write("Try Something else")
+      }
+  
